2a06205112043775ec8d4c51e128d99491e04858
기범 <kibeom.youn@gmail.com>
2019-02-02 11:10:29 +0900

    Save .mcs file list to Excel file

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace MCSFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dirLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnDir = new System.Windows.Forms.Button();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.lstFileView = new System.Windows.Forms.ListView();
+            this.colDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colExtension = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblRegex = new System.Windows.Forms.Label();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(754, 6);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(158, 62);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Excel 저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dirLabel
+            // 
+            this.dirLabel.AutoSize = true;
+            this.dirLabel.Location = new System.Drawing.Point(12, 11);
+            this.dirLabel.Name = "dirLabel";
+            this.dirLabel.Size = new System.Drawing.Size(86, 18);
+            this.dirLabel.TabIndex = 1;
+            this.dirLabel.Text = "검색 경로";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 45);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(86, 18);
+            this.resultLabel.TabIndex = 1;
+            this.resultLabel.Text = "찾을 파일";
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(104, 6);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(401, 28);
+            this.txtDir.TabIndex = 2;
+            // 
+            // btnDir
+            // 
+            this.btnDir.Location = new System.Drawing.Point(511, 6);
+            this.btnDir.Name = "btnDir";
+            this.btnDir.Size = new System.Drawing.Size(75, 28);
+            this.btnDir.TabIndex = 3;
+            this.btnDir.Text = "찾기";
+            this.btnDir.UseVisualStyleBackColor = true;
+            this.btnDir.Click += new System.EventHandler(this.btnDir_Click);
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(104, 40);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(196, 28);
+            this.txtWord.TabIndex = 4;
+            this.txtWord.Text = "*.mcs";
+            // 
+            // lstFileView
+            // 
+            this.lstFileView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colPath,
+            this.colDate,
+            this.colName,
+            this.colSize,
+            this.colExtension});
+            this.lstFileView.GridLines = true;
+            this.lstFileView.Location = new System.Drawing.Point(12, 74);
+            this.lstFileView.Name = "lstFileView";
+            this.lstFileView.Size = new System.Drawing.Size(900, 474);
+            this.lstFileView.TabIndex = 5;
+            this.lstFileView.UseCompatibleStateImageBehavior = false;
+            this.lstFileView.View = System.Windows.Forms.View.Details;
+            // 
+            // colDate
+            // 
+            this.colDate.Text = "날짜";
+            this.colDate.Width = 100;
+            // 
+            // colName
+            // 
+            this.colName.Text = "이름";
+            this.colName.Width = 120;
+            // 
+            // colPath
+            // 
+            this.colPath.Text = "경로";
+            this.colPath.Width = 300;
+            // 
+            // colSize
+            // 
+            this.colSize.Text = "파일 용량";
+            this.colSize.Width = 130;
+            // 
+            // colExtension
+            // 
+            this.colExtension.Text = "종류";
+            this.colExtension.Width = 80;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(592, 6);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(156, 62);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblRegex
+            // 
+            this.lblRegex.AutoSize = true;
+            this.lblRegex.Location = new System.Drawing.Point(317, 45);
+            this.lblRegex.Name = "lblRegex";
+            this.lblRegex.Size = new System.Drawing.Size(66, 18);
+            this.lblRegex.TabIndex = 7;
+            this.lblRegex.Text = "Reg Ex";
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new System.Drawing.Point(389, 40);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(197, 28);
+            this.txtRegex.TabIndex = 8;
+            this.txtRegex.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 560);
+            this.Controls.Add(this.txtRegex);
+            this.Controls.Add(this.lblRegex);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lstFileView);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.btnDir);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.dirLabel);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label dirLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnDir;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.ListView lstFileView;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.ColumnHeader colDate;
+        private System.Windows.Forms.ColumnHeader colPath;
+        private System.Windows.Forms.ColumnHeader colSize;
+        private System.Windows.Forms.ColumnHeader colExtension;
+        private System.Windows.Forms.Label lblRegex;
+        private System.Windows.Forms.TextBox txtRegex;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using System.Text.RegularExpressions;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MCSFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] colList = { "날짜", "이름", "경로", "용량", "종류" };
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (lstFileView.Items.Count < 1)
+            {
+                MessageBox.Show("목록이 비었습니다.");
+                return;
+            }
+
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook wb = excelApp.Workbooks.Add();            // Sheet1이 포함된 빈 workbook 생성
+            Excel.Worksheet ws1 = wb.Worksheets.Item["Sheet1"];       // Sheet1 가져오기
+            //Excel.Worksheet ws2 = wb.Worksheets.Add(After: ws1);        // ws1 뒤에 새 워크시트 생성
+
+            // 열 이름 붙이기
+            for (int i = 1; i <= colList.Length; i++)
+                ws1.Cells[1, i] = colList[i - 1];
+            
+            // 항목 채워넣기
+            for(int row = 1; row <= lstFileView.Items.Count; row++)
+            {
+                for(int col = 1; col <= 5; col++)
+                {
+                    ws1.Cells[row + 1, col] = lstFileView.Items[row - 1].SubItems[col - 1].Text;
+                }
+            }
+
+            //Excel.Range range = ws2.Cells[1, 1];
+            //range.Formula = "=SUM(Sheet1!$A$1:$E$5)";   // 셀에 수식 기록
+
+            //range = ws2.Cells[2, 1];
+            //range.Formula = "=AVERAGE(Sheet1!A1:E5)";
+
+            SaveFileDialog saveFileDlg = new SaveFileDialog();
+            saveFileDlg.Title = "파일 저장";
+            saveFileDlg.OverwritePrompt = true;
+            saveFileDlg.Filter = "Excel File(*.xlsx)|.xlsx";
+
+            if (saveFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                wb.SaveAs(Filename: saveFileDlg.FileName);
+                wb.Close();
+            }
+        }
+
+        private void btnDir_Click(object sender, EventArgs e)
+        {
+            // 폴더브라우저 객체 생성
+            FolderBrowserDialog dir = new FolderBrowserDialog();
+            if (dir.ShowDialog() == DialogResult.OK)
+            {
+                this.txtDir.Text = dir.SelectedPath.Trim(); //선택한 디렉토리 경로
+            }
+
+            // 포커스 단어 입력창으로 이동
+            txtWord.Focus();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // 검색을 위한 체크
+            if (txtWord.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("검색단어를 입력해주세요.");
+                return;
+            }
+
+            if (txtDir.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("검색할 디렉토리를 입력해주세요.");
+                return;
+            }
+            // 검색
+            lstFileView.Items.Clear();
+            FindFile(txtDir.Text.Trim(), txtWord.Text.Trim());
+
+            txtWord.Focus();
+        }
+
+        private void FindFile(string dir, string word)
+        {
+            // 파일 목록 저장
+            // 선택한 directory의 하위 dir 파일까지 모두 검색함
+            string[] files = Directory.GetFiles(dir, word, SearchOption.AllDirectories);
+            
+            // 파일 개수만큼 리스트에 추가
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo fileInfo = new FileInfo(files[i]);
+
+                Regex regex = new Regex(txtRegex.Text);
+
+                // 입력 기호로 split -> 일치하는 기호가 없다면 원래 파일명 하나 반환
+                // 파일 이름이 날짜-이름.mcs 형식이거나 특정한 규칙 없이 만들어져 있음. 전자는 vals가 2개, 후자는 1개의 item을 가짐
+                string[] vals = regex.Split(Path.GetFileNameWithoutExtension(fileInfo.FullName));
+
+                // 경로 / 날짜 / 이름 / 파일크기 / 종류 
+                ListViewItem item = new ListViewItem(fileInfo.FullName);
+                switch (vals.Length)
+                {
+                    case 1:
+                        item.SubItems.Add("");
+                        item.SubItems.Add(vals[0]);
+                        break;
+                    case 2:
+                        item.SubItems.Add(vals[0]);
+                        item.SubItems.Add(vals[1]);
+                        break;
+                }
+                item.SubItems.Add(fileInfo.Length.ToString());
+                item.SubItems.Add(fileInfo.Extension.ToString());
+                lstFileView.Items.Add(item);
+
+            }
+        }
+    }
+}
